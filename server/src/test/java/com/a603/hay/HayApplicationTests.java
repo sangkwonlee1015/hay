1787616a058bd71b281a91462f052ec704428e44
jonghwi <orien93@naver.com>
2022-10-24 13:37:20 +0900

    [init]init

---
+++ server/src/test/java/com/a603/hay/HayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a603.hay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
