b682be53da2a14b4e434d2fad071f370d3542450
이상권 <copeve@naver.com>
2022-11-04 17:53:08 +0900

    [test] MainPage voteList 테스트

--- front/src/components/VoteItem.jsx
+++ front/src/components/VoteItem.jsx
@@ -1,12 +1,18 @@
 // 누르면 투표글로 이동하는 투표 요약 컴포넌트
 // 아이콘(베스트일 경우 왕관), 제목, 등록일자, 남은날짜, 참여자 수 포함
 
-import React from 'react'
+import React from "react";
 
-function VoteItem() {
+function VoteItem({ vote }) {
   return (
-    <div>VoteItem</div>
-  )
+    <div>
+      {vote.title}
+      <br />
+      {vote.startDate}, {vote.endDate}
+      <br />
+      {vote.voteCount}
+    </div>
+  );
 }
 
-export default VoteItem
+export default VoteItem;
--- front/src/pages/MainPage/MainPage.jsx
+++ front/src/pages/MainPage/MainPage.jsx
@@ -5,12 +5,35 @@
 // 투표목록 (바깥 components 폴더에 있음)
 // 하단바 (components 폴더에 있음)
 
-import React from 'react'
-import Banner from './Banner'
-import Category from './Category'
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import api from "../../api/api";
+import VoteItem from "../../components/VoteItem";
+import Banner from "./Banner";
+import Category from "./Category";
 
 function MainPage() {
+  const [voteList, setVoteList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("api/votes?order=최신", {
+        headers: {
+          Authorization:
+            "Bearer " +
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJzX2t6eGN2QG5hdmVyLmNvbSIsIm5pY2tuYW1lIjoi7J207IOB6raMIiwiaXNzIjoiaGF5IiwiZXhwIjoxNjY4MTQ5NjAzfQ.Livkg_-eWhsS-L00xGCSHLjMwuNEMNr2H2ZJnbAcsDo",
+        },
+      })
+      .then(({ data }) => {
+        setVoteList(data.response);
+        console.log(data.response);
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  }, []);
+
   return (
     <>
       <div>MainPage</div>
@@ -18,8 +41,11 @@ function MainPage() {
       <Category name="먹자지껄" />
       <Category name="매일매일" />
       <Banner />
+      {voteList.map((vote, index) => (
+        <VoteItem vote={vote}></VoteItem>
+      ))}
     </>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
