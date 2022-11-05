5af6f1c79d6a2b20f2d03e5c2dab89ffe27861a9
bmyusharp <1004romeo@naver.com>
2022-11-05 12:13:43 +0900

    [develop] Add function remainTime - 남은 일자 계산하는 함수 새로 만듦 - css 파일 추가

--- front/src/pages/VoteDetail/VoteDetail.jsx
+++ front/src/pages/VoteDetail/VoteDetail.jsx
@@ -14,10 +14,12 @@
 
 import { Details, DetailsSharp } from '@mui/icons-material'
 import React from 'react'
+import './VoteDetail.css';
 
 function VoteDetail() {
   
   const details = {
+    id: 1,
     title: "여러분이 정해주신 곳으로 가겠습니다.",
     작성자: "건설로봇",
     distanceLevel: 0,
@@ -25,7 +27,7 @@ function VoteDetail() {
     body: "안녕하세요 저는 매일매일 노동력을 착취당하는 프롤레탈리아입니...",
     voteCount: 10,
     startDate: "2022-10-28 14:01:00.000000",
-    endDate: "2022-11-04 14:01:00.000000",
+    endDate: "2022-11-30 11:05:59.000000",
     ended: false,
     voted: true,
     voteItems: [
@@ -93,7 +95,7 @@ function VoteDetail() {
   return (
     <>
       <div>
-        <div>{details.title}</div>
+        <h1>{details.title}</h1>
         <div>{details.작성자}</div>
         <div>{details.distanceLevel}</div>
         <div>{details.startDate}</div>
@@ -101,19 +103,23 @@ function VoteDetail() {
       <div>이미지</div>
       <div>{details.body}</div>
       <div>
-        투표
+        <div>투표 {details.voteCount}명 참여</div>
+        <div>{남은날짜계산(details.endDate)}</div>
       </div>
       댓글 {commentCount()}      <div>공유하기</div>
       <div>
         <h2>베스트 댓글</h2>
         <div></div>
 
-        <div>
-          <div>{details.comments[0].content}</div>
-          <div>{details.comments[0].writerNickname}</div>
-          <div>좋아요 {details.comments[0].likesCount}</div>
-          <div>{details.comments[0].createdAt}</div>
-        </div>
+        {details.comments[0].deleted 
+        ? <div>삭제된 댓글입니다.</div>
+        : <div>
+            <div>{details.comments[0].content}</div>
+            <div>{details.comments[0].writerNickname}</div>
+            <div>좋아요 {details.comments[0].likesCount}</div>
+            <div>{details.comments[0].createdAt}</div>
+          </div>
+        }
       </div>
     </>
   )
@@ -126,28 +132,55 @@ export default VoteDetail
    * 종료시점과 현재날짜를 비교하여 남은 날짜 또는 시간을 보여주거나 종료된 투표임을 반환하는 함수.
    * @returns {string}
    */
- const 남은날짜계산 = (endTime) => {
-  let todayRaw = new Date();
-  let today = todayRaw.getFullYear() + "-" + todayRaw.getMonth() + "-" + todayRaw.getDate() + "T" + todayRaw.getHours() + ":" + todayRaw.getMinutes() + ":" + todayRaw.getSeconds()
-  let result = 0;
+//  const 남은날짜계산1 = (endTime) => {
+//   let todayRaw = new Date();
+//   let today = todayRaw.getFullYear() + "-" + todayRaw.getMonth() + "-" + todayRaw.getDate() + " " + todayRaw.getHours() + ":" + todayRaw.getMinutes() + ":" + todayRaw.getSeconds() + ".000000";
+//   let result = 0;
 
-  if (today > endTime) {
-    // 투표 종료된 이후
-    result = "종료된 투표";
-  } else if (today.substring(5, 7) < endTime.substring(5, 7)) { // 예외처리를 월 차이만 하자.
-    // 다음 달까지 넘어감
-    result = new Date(endTime.substring(0, 4), endTime.substring(5, 7), 0).getDate() + endTime.substring(8, 10) - today.substring(8, 10);
-    result = result + "일 남음";
-  } else if (endTime.substring(8, 10) - today.substring(8, 10) < 1) {
-    // 하루보다 짧은 시간일 경우
-    result = endTime.substring(11, 13) - today.substring(11, 13);
-    result = result + "시간 남음";
+//   if (today > endTime) {
+//     // 투표 종료된 이후
+//     result = "종료된 투표";
+//   } else if (today.substring(5, 7) < endTime.substring(5, 7)) { // 예외처리를 월 차이만 하자.
+//     // 다음 달까지 넘어감
+//     result = new Date(2022/*parseInt(endTime.substring(0, 4))*/, parseInt(endTime.substring(5, 7)), 0).getDate() + parseInt(endTime.substring(8, 10)) - parseInt(today.substring(8, 10));
+//     result = result + "일 후 종료됩니다.";
+//   } else if (parseInt(endTime.substring(8, 10)) - parseInt(today.substring(8, 10)) < 1) {
+//     // 하루보다 짧은 시간일 경우
+//     result = parseInt(endTime.substring(11, 13)) - parseInt(today.substring(11, 13));
+//     result = result + "시간 후 종료됩니다.";
+//   } else {
+//     result = parseInt(endTime.substring(8, 10)) - parseInt(today.substring(8, 10));
+//     result = result + "일 후 종료됩니다.";
+//   }
+
+//   return result;
+// }
+
+/**
+ * 종료시점과 현재날짜를 비교하여 남은 날짜 또는 시간을 보여주거나 종료된 투표임을 반환하는 함수.
+ * @returns {string}
+ */
+const 남은날짜계산 = (endTime) => {
+  const today = new Date();
+  const end = new Date(endTime);
+  const diff = end - today;
+  const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const diffHours = Math.floor(diff / (1000 * 60 * 60));
+  const diffMinutes = Math.floor(diff / (1000 * 60));
+  
+  if (end < today) {
+    return "종료된 투표";
+  } else if (diffHours < 1) {
+    if (diffMinutes < 5) {
+      return "잠시 후 종료됩니다.";
+    } else {
+      return `${diffMinutes}분 후 종료됩니다.`;
+    }
+  } else if (diffDays < 1) {
+    return `${diffHours}시간 후 종료됩니다.`;
   } else {
-    result = endTime.substring(8, 10) - today.substring(8, 10);
-    result = result + "일 남음";
+    return `${diffDays}일 후 종료됩니다.`;
   }
-
-  return result;
 }
 
 /**
