8306a6663c9f7b7fe4a19da2fe0cc8d57e6e812b
Kim <rdt2359@gmail.com>
2022-11-08 17:04:04 +0900

    [fix[ 카카오 로그인 추가 정보 입력 버그 수정

--- front/src/pages/User/Login.jsx
+++ front/src/pages/User/Login.jsx
@@ -5,15 +5,15 @@
 import React from 'react'
 import logo from '../../img/logo.png';
 import kakao_login from '../../img/kakao_login.png'
-
-const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=2cc38f3feb14c46b190ca5fe77598eb6&redirect_uri=http://localhost:3000/kakaologin&response_type=code`;
+import api from '../../api/api';
 
 function Login() {
+  console.log(api)
   return (
     <>
       <img src={logo} alt='너는어때 로고' width='300px'/>
       <div>너는어때</div>
-        <a href={KAKAO_AUTH_URL}>
+        <a href={api.kakaoLogin()}>
          <img src={kakao_login} alt='카카오로그인'/>
         </a>
     </>
