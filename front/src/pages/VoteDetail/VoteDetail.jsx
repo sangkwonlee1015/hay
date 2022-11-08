226cdd35ae3af565ac952ab2c2f8d11d16ec6ca8
Bermyu <1004romeo@naver.com>
2022-11-08 11:43:49 +0900

    [chore] style progressing

--- front/src/pages/VoteDetail/VoteDetail.jsx
+++ front/src/pages/VoteDetail/VoteDetail.jsx
@@ -12,13 +12,13 @@
 // 댓글 수, 공유하기
 // 댓글 리스트
 
-import { Details, DetailsSharp } from '@mui/icons-material'
-import { borderRadius } from '@mui/system';
-import React from 'react'
-import './VoteDetail.css';
+import HowToVoteIcon from '@mui/icons-material/HowToVote';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import React from "react";
+import "./VoteDetail.css";
 
 function VoteDetail() {
-  
   const details = {
     id: 1,
     title: "여러분이 정해주신 곳으로 가겠습니다.",
@@ -28,7 +28,7 @@ function VoteDetail() {
     body: "안녕하세요 저는 매일매일 노동력을 착취당하는 프롤레탈리아입니...",
     voteCount: 10,
     startDate: "2022-10-28 14:01:00.000000",
-    endDate: "2022-11-30 11:05:59.000000",
+    endDate: "2022-11-21 00:05:59.000000",
     ended: false,
     voted: true,
     voteItems: [
@@ -73,7 +73,7 @@ function VoteDetail() {
         ],
       },
     ],
-  }
+  };
 
   /**
    * 댓글별로 대댓글까지 순회하며 카운트해서 총 댓글 수를 반환하는 함수
@@ -89,8 +89,8 @@ function VoteDetail() {
       }
       result += 1;
     }
-    return result
-  }
+    return result;
+  };
 
   /**
    * 투표거리에 따른 단계(스타일 적용된)
@@ -98,19 +98,64 @@ function VoteDetail() {
   let distanceLevel = null;
   switch (details.distanceLevel) {
     case 0: {
-      distanceLevel = <div style={{backgroundColor:'e7f3fe', fontSize:'12px', fontWeight:'bold', borderRadius:'4px'}}>{details.distanceLevel}</div>;
+      distanceLevel = (
+        <div
+          style={{
+            backgroundColor: "e7f3fe",
+            fontSize: "12px",
+            fontWeight: "bold",
+            borderRadius: "4px",
+          }}
+        >
+          {details.distanceLevel}
+        </div>
+      );
       break;
     }
     case 1: {
-      distanceLevel = <div style={{backgroundColor:'cfe8fc', fontSize:'12px', fontWeight:'bold', borderRadius:'4px'}}>{details.distanceLevel}</div>;
+      distanceLevel = (
+        <div
+          style={{
+            backgroundColor: "cfe8fc",
+            fontSize: "12px",
+            fontWeight: "bold",
+            borderRadius: "4px",
+          }}
+        >
+          {details.distanceLevel}
+        </div>
+      );
       break;
     }
     default: {
-      distanceLevel = <div style={{backgroundColor:'b6dcfb', fontSize:'12px', fontWeight:'bold', borderRadius:'4px'}}>{details.distanceLevel}</div>;
+      distanceLevel = (
+        <div
+          style={{
+            backgroundColor: "b6dcfb",
+            fontSize: "12px",
+            fontWeight: "bold",
+            borderRadius: "4px",
+          }}
+        >
+          {details.distanceLevel}
+        </div>
+      );
     }
   }
-  
-    
+
+  /**
+   * 투표 선택지 반복랜더링
+   */
+  const selectionGroup = () => {
+    return (
+      <div>
+        {details.voteItems.map((selection)=>(
+          <div className={ selection.voted ? "selected" : "selection" }>{selection.voted ? <RadioButtonCheckedIcon color='primary'/> : <RadioButtonUncheckedIcon/>}{selection.content}</div>
+        ))}
+      </div>
+    )
+  }
+
   return (
     <div className="contentAll">
       <div>
@@ -118,17 +163,19 @@ function VoteDetail() {
         <div className="titleGroup">
           <div className="writter">{details.작성자}</div>
           {distanceLevel}
-          <div className="startDate">{details.startDate.substring(0,16)}</div>
+          <div className="startDate">{details.startDate.substring(0, 16)}</div>
         </div>
       </div>
       <div>이미지</div>
       <div className="article">{details.body}</div>
       <div className="vote">
         <div className="voteTitle">
+          <HowToVoteIcon color="primary" fontSize="small"/>
           <div className="voteText">투표</div>
           <div>{details.voteCount}명 참여</div>
         </div>
         <div className="remainDate">{남은날짜계산(details.endDate)}</div>
+        <div className="selectionGroup">{selectionGroup()}</div>
       </div>
       <div className="commentShare">
         <div className="commentNav">댓글 {commentCount()}</div>
@@ -138,27 +185,27 @@ function VoteDetail() {
         <h2>베스트 댓글</h2>
         <div></div>
 
-        {details.comments[0].deleted 
-        ? <div>삭제된 댓글입니다.</div>
-        : <div>
+        {details.comments[0].deleted ? (
+          <div>삭제된 댓글입니다.</div>
+        ) : (
+          <div>
             <div>{details.comments[0].content}</div>
             <div>{details.comments[0].writerNickname}</div>
             <div>좋아요 {details.comments[0].likesCount}</div>
-            <div>{details.comments[0].createdAt.substring(0,16)}</div>
+            <div>{details.comments[0].createdAt.substring(0, 16)}</div>
           </div>
-        }
+        )}
       </div>
     </div>
-  )
+  );
 }
 
-export default VoteDetail
-
+export default VoteDetail;
 
 /**
-   * 종료시점과 현재날짜를 비교하여 남은 날짜 또는 시간을 보여주거나 종료된 투표임을 반환하는 함수.
-   * @returns {string}
-   */
+ * 종료시점과 현재날짜를 비교하여 남은 날짜 또는 시간을 보여주거나 종료된 투표임을 반환하는 함수.
+ * @returns {string}
+ */
 //  const 남은날짜계산1 = (endTime) => {
 //   let todayRaw = new Date();
 //   let today = todayRaw.getFullYear() + "-" + todayRaw.getMonth() + "-" + todayRaw.getDate() + " " + todayRaw.getHours() + ":" + todayRaw.getMinutes() + ":" + todayRaw.getSeconds() + ".000000";
@@ -194,7 +241,7 @@ const 남은날짜계산 = (endTime) => {
   const diffDays = Math.floor(diff / (1000 * 60 * 60 * 24));
   const diffHours = Math.floor(diff / (1000 * 60 * 60));
   const diffMinutes = Math.floor(diff / (1000 * 60));
-  
+
   if (end < today) {
     return "종료된 투표";
   } else if (diffHours < 1) {
@@ -208,17 +255,17 @@ const 남은날짜계산 = (endTime) => {
   } else {
     return `${diffDays}일 후 종료됩니다.`;
   }
-}
+};
 
 /**
-   * 해당 투표와 조회한 사람의 거리를 0.5km, 1km로 끊어 어느 정도 가까운지를 반환하는 함수.
-   * 
-   * 최대 조회 거리가 2km임을 감안하여 1km 이상의 거리는 별도로 끊지 않았음에 주의
-   * @param {number} distance
-   * @returns {string}
-   */
- const 거리계산 = (distance) => {
-  let result = ""
+ * 해당 투표와 조회한 사람의 거리를 0.5km, 1km로 끊어 어느 정도 가까운지를 반환하는 함수.
+ *
+ * 최대 조회 거리가 2km임을 감안하여 1km 이상의 거리는 별도로 끊지 않았음에 주의
+ * @param {number} distance
+ * @returns {string}
+ */
+const 거리계산 = (distance) => {
+  let result = "";
   if (distance < 500) {
     result = "0.5km 이내";
   } else if (distance < 1000) {
@@ -227,4 +274,4 @@ const 남은날짜계산 = (endTime) => {
     result = "2km 이내";
   }
   return result;
-}
+};
