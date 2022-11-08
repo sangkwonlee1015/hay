33dd55dc15751d9096f8e20236c49217e1959a71
이상권 <copeve@naver.com>
2022-11-08 15:26:30 +0900

    [feature] path 추가

--- front/src/index.js
+++ front/src/index.js
@@ -1,30 +1,30 @@
-import React from "react";
+import React from 'react';
 import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import store from "./_store/store";
 
-import App from "./App";
-import Login from "./pages/User/Login";
-import KakaoLogin from "./pages/User/KakaoLogin";
-import SignUp from "./pages/User/Signup";
-import LocationSetting from "./pages/User/LocationSetting";
-import MainPage from "./pages/MainPage/MainPage";
-import Mypage from "./pages/MyPage/MyPage";
-import MyLocationSetting from "./pages/MyPage/MyLocationSetting";
-import VoteDetail from "./pages/VoteDetail/VoteDetail";
-import VoteCreate from "./pages/VoteDetail/VoteCreate";
-import MyVotePage from "./pages/MyPage/MyVotePage";
-import MyParticipatedPage from "./pages/MyPage/MyParticipatedPage";
+import App from './App';
+import Login from './pages/User/Login';
+import KakaoLogin from './pages/User/KakaoLogin';
+import SignUp from './pages/User/Signup';
+import LocationSetting from './pages/User/LocationSetting';
+import MainPage from './pages/MainPage/MainPage';
+import Mypage from './pages/MyPage/MyPage';
+import MyLocationSetting from './pages/MyPage/MyLocationSetting';
+import VoteDetail from './pages/VoteDetail/VoteDetail';
+import VoteCreate from './pages/VoteDetail/VoteCreate';
+import MyVotePage from './pages/MyPage/MyVotePage';
+import MyParticipatedPage from './pages/MyPage/MyParticipatedPage';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
       <Routes>
         <Route path="/" element={<App />}>
-          <Route path="" element={<Login />} />
+          <Route path="" element={<Login />} /> 
           <Route path="login" element={<Login />} />
           <Route path="kakaologin" element={<KakaoLogin />} />
           <Route path="signup" element={<SignUp />} />
