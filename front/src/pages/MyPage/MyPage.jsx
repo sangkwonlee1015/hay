cdb7db63735564f9730a0665e2f22deaac3de448
Bermyu <1004romeo@naver.com>
2022-11-10 17:42:02 +0900

    [style] Style myPage - 페이지 설정 - 닉네임 공간 배치 - 네비게이션 메뉴 디자인 - 로그아웃 버튼 배치&디자인

--- front/src/pages/MyPage/MyPage.jsx
+++ front/src/pages/MyPage/MyPage.jsx
@@ -6,10 +6,13 @@
 // 하단 바 (components에 있음)
 
 import React from 'react'
-import { Button } from "@mui/material";
+// import { Button } from "@mui/material";
 import {useNavigate} from 'react-router-dom';
 import HeaderOnlyText from '../../components/HeaderOnlyText';
 import setAuthorizationToken from '../User/AuthorizationToken';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import jwt_decode from "jwt-decode";
+import "./MyPage.css";
 
 function MyPage() {
   const navigate = useNavigate();
@@ -20,61 +23,38 @@ function MyPage() {
     localStorage.removeItem('jwtToken')
   }
 
-  function moveToLocationSetting() {
-    navigate("/mylocationsetting");
-  }
-
-  function moveToMyParticipated() {
-    navigate("/myparticipated");
-  }
-
-  function moveToMyVote() {
-    navigate("/myvote");
+  const getNickname = () => {
+    let nickName = jwt_decode(localStorage.getItem('jwtToken')).nickname
+    return nickName
   }
 
   return (
-    <>
-      <div>
-        <HeaderOnlyText text="마이페이지" />
-      </div>
-      <div className="myNickname">
-        닉네임
-        <button>수정</button>
-      </div>
-      <div className="myInfoList">
-        내가 생성한 투표
-        <button 
-          className="myCreateVote"
-          onClick={() => {
-            moveToMyVote()
-          }}
-          >{'>'}</button><br></br>
-        내가 참여한 투표
-        <button 
-          className="myJoinVote"
-          onClick={() => {
-            moveToMyParticipated()
-          }}
-          >{'>'}</button><br></br>
-        내 동네 설정
-        <button 
-          className="myLocation"
-          onClick={() => {
-            moveToLocationSetting()
-          }}
-          >{'>'}</button><br></br>
-      </div>
-      <div>
-        <Button
-          variant="log-out"
-          onClick={() => {
-            logout()
-          }}
-        >
+    <div>
+      <HeaderOnlyText text="마이페이지" />
+      <div className="myPage">
+        <div className="myNickname">
+          아주강력한닉네임
+          <button>수정</button>
+        </div>
+        <div className="myInfoList">
+          <div onClick={() => {navigate("/myvote")}} className="myPageNavButton">
+            <div>내가 생성한 투표</div>
+            <div className="myPageNavButtonRightIcon"><ChevronRightIcon /></div>
+          </div>
+          <div onClick={() => {navigate("/myparticipated")}} className="myPageNavButton">
+            <div>내가 참여한 투표</div>
+            <div className="myPageNavButtonRightIcon"><ChevronRightIcon /></div>
+          </div>
+          <div onClick={() => {navigate("/mylocationsetting")}} className="myPageNavButton">
+            <div>내 동네 설정</div>
+            <div className="myPageNavButtonRightIcon"><ChevronRightIcon /></div>
+          </div>
+        </div>
+        <div onClick={logout} className="myPageLogoutButton">
           로그아웃
-        </Button>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
