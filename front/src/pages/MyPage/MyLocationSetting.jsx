8124c69959b0826463b07921435af00e127abd31
Bermyu <1004romeo@naver.com>
2022-11-10 14:14:25 +0900

    [chore] style locate chip & area range slide - 동네 선택 버튼, 거리 지정 슬라이더 디자인

--- front/src/pages/MyPage/MyLocationSetting.jsx
+++ front/src/pages/MyPage/MyLocationSetting.jsx
@@ -13,6 +13,9 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { Button } from "@mui/material";
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
 import AutorenewIcon from "@mui/icons-material/Autorenew";
 import Map from '../../components/Map'
 import HeaderOnlyText from '../../components/HeaderOnlyText';
@@ -20,6 +23,7 @@ import { userAction } from "../../_slice/UserSlice";
 import { useLocation } from "react-router";
 import axios from 'axios';
 import api from '../../api/api';
+import "./MyLocationSetting.css";
 
 function MyLocationSetting() {
   const [position, setPosition] = useState({});
@@ -127,12 +131,79 @@ function MyLocationSetting() {
   dispatch(userAction.latitude(position.latitude));
   dispatch(userAction.longitude(position.longitude));
 
+  const 동네이름 = [
+    {
+      name: "삼선동 5가",
+      location: {
+        x: 127.0396789,
+        y: 37.5013305,
+      },
+      isSelected: true,
+    },
+    {
+      name: "선택하여 설정",
+      location: {
+        x: 0,
+        y: 0,
+      },
+      isSelected: false,
+    },
+  ]
+  const valuetext = (value) => {
+    return `${value}km`;
+  }
+  const marks = [
+    {
+      value: 0.5,
+      label: "0.5km",
+    },
+    {
+      value: 1,
+      label: "1km",
+    },
+    {
+      value: 2,
+      label: "2km",
+    }
+  ]
+  const valueLabelFormat = (value) => {
+    return marks.findIndex((mark) => mark.value === value) + 1;
+  }
+  
   return (
-    <>
+    <div className="myPage">
       <HeaderTextAndNavigate path="/mypage" text="내 동네 설정" />
       <Map />
       <div>{`위도 ${latitude}   경도 ${longitude}`}</div>
       <div>{areaName}</div>
+      <Stack direction="row" spacing={2} justifyContent="center">
+        <Chip
+          label={동네이름[0].name}
+          onClick={setCurrentLocation}
+          onDelete={deleteMyLocation}
+          color="primary"
+          variant={ 동네이름[0].isSelected ? "filled" : "outlined" }
+        />
+        <Chip
+          label={동네이름[1].name}
+          onClick={setCurrentLocation}
+          onDelete={deleteMyLocation}
+          color="primary"
+          variant={ 동네이름[1].isSelected ? "filled" : "outlined" }
+        />
+      </Stack>
+      <div className="slider">
+        <Slider
+          aria-label="Restricted values"
+          defaultValue={2}
+          valueLabelFormat={valueLabelFormat}
+          getAriaValueText={valuetext}
+          step={null}
+          max={2}
+          valueLabelDisplay="off"
+          marks={marks}
+        />
+      </div>
       <div onClick={handleRenew}>
         <AutorenewIcon sx={{ fontSize: 50 }} />
       </div>
@@ -159,7 +230,7 @@ function MyLocationSetting() {
         test03
       </Button>
 
-    </>
+    </div>
   )
 }
 
